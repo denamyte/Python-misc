5bc2afb2bfc6103673fc019208300d7381287d16
denamyte <denamyte@gmail.com>
2021-03-13 20:12:20 +0200

    2021-03-13: leetcode problem #00142: solved

--- leetcode/001/code00142.py
+++ leetcode/001/code00142.py
@@ -1,12 +1,75 @@
-from typing import Optional
+from typing import Optional, Union
 
 
 class ListNode:
-    def __init__(self, x):
+    def __init__(self, x, nextRef=None):
         self.val = x
-        self.next: Optional[ListNode] = None
+        self.next: Optional[ListNode] = nextRef
 
 
 class Solution:
-    def detectCycle(self, head: ListNode) -> ListNode:
-        pass
+    def detectCycle(self, head: ListNode) -> Union[ListNode, None]:
+        single = head.next if head else None
+        double = single.next if single else None
+        while double is not None and single != double:
+            single = single.next
+            double = double.next
+            if double:
+                double = double.next
+        if not double:
+            return None
+
+        single = head
+        while single != double:
+            single = single.next
+            double = double.next
+        return single
+
+    def detectCycle2(self, head: ListNode) -> Union[ListNode, None]:
+        single = head.next if head else None
+        double = single.next if single else None
+        while True:
+            if not double or not double.next:
+                return None
+            double = double.next.next
+            single = single.next
+            if single == double:
+                break
+
+        single = head
+        while single != double:
+            single = single.next
+            double = double.next
+        return single
+
+
+index = 0
+
+
+def test(node: ListNode, expectedConnection: Union[ListNode, None]):
+    global index
+    index += 1
+    connection = Solution().detectCycle2(node)
+    print(f'''Test #{index}
+----------------
+expected connection matches: {connection is expectedConnection}
+''')
+
+
+node = ListNode(3, ListNode(2, ListNode(0, ListNode(-4))))
+connection = node.next
+node.next.next.next.next = connection
+test(node, connection)
+
+node = ListNode(1, ListNode(2))
+connection = node
+node.next.next = connection
+test(node, connection)
+
+node = ListNode(1)
+test(node, None)
+
+node = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, ListNode(6))))))
+connection = node.next.next
+node.next.next.next.next.next.next = connection
+test(node, connection)
